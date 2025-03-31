e5e986deef33e37cbc959f55ec3bfc6bfa2fdba0
Manuel Maquin <manuelzuniga005@gmail.com>
2025-03-31 15:54:05 -0600

    :heavy_plus_sign: Agregando frontend_congreso

--- frontend_congreso/src/App.jsx
+++ frontend_congreso/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Route, Routes } from 'react-router-dom'
+import HomePage from "./pages/HomePage"
+import BlogPage from "./pages/BlogPage"
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-    <>
+    /*<>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +30,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </>*/
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<BlogPage />} />
+    </Routes>
+    
   )
 }
 
