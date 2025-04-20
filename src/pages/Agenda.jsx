cc80145dabd792880a713033989b2d10d31c892f
anthonymateo <anthonymateodelosreyes.18@gmail.com>
2025-04-19 20:13:51 -0600

    Arreglo del temporizador

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -3,23 +3,46 @@ import '../styles/Agenda.css';
 
 const Agenda = () => {
   const agendaData = {
-    "Viernes 16": [
-      { hora: "09:00 - 10:00", taller: "Taller 1", conferencista: "Nombre 1", salon: "Salon 1" },
-      { hora: "10:15 - 11:15", taller: "Taller ", conferencista:  "Nombre 2", salon:  "Salon 2" },
+    'Viernes 16': [
+      {
+        hora: '09:00 - 10:00',
+        taller: 'Taller 1',
+        conferencista: 'Nombre 1',
+        salon: 'Salon 1',
+      },
+      {
+        hora: '10:15 - 11:15',
+        taller: 'Taller ',
+        conferencista: 'Nombre 2',
+        salon: 'Salon 2',
+      },
     ],
-    "Sábado 17": [
-      { hora: "09:00 - 10:00", taller: "Taller 3", conferencista: "Nombre 3", salon: "Salon 3" },
-      { hora: "10:15 - 11:15", taller: "Taller 4", conferencista: "Nombre 4", salon: "Salon 4" },
-    ]
+    'Sábado 17': [
+      {
+        hora: '09:00 - 10:00',
+        taller: 'Taller 3',
+        conferencista: 'Nombre 3',
+        salon: 'Salon 3',
+      },
+      {
+        hora: '10:15 - 11:15',
+        taller: 'Taller 4',
+        conferencista: 'Nombre 4',
+        salon: 'Salon 4',
+      },
+    ],
   };
 
   return (
-    <div className="agenda-container">
-      <h2 className="agenda-title">AGENDA</h2>
+    <div className='agenda-container'>
+      <h2 className='agenda-title'>AGENDA</h2>
       {Object.entries(agendaData).map(([dia, actividades]) => (
-        <div key={dia} className="agenda-dia">
+        <div
+          key={dia}
+          className='agenda-dia'
+        >
           <h3>{dia}</h3>
-          <table className="agenda-tabla">
+          <table className='agenda-tabla'>
             <thead>
               <tr>
                 <th>Hora</th>
@@ -31,14 +54,13 @@ const Agenda = () => {
             <tbody>
               {actividades.map((item, index) => (
                 <tr key={index}>
-                  <td className="agenda-hora">{item.hora}</td>
-                  <td className="agenda-taller">{item.taller}</td>
-                  <td className="agenda-conferencista">{item.conferencista}</td>
-                  <td className="agenda-salon">{item.salon}</td>
+                  <td className='agenda-hora'>{item.hora}</td>
+                  <td className='agenda-taller'>{item.taller}</td>
+                  <td className='agenda-conferencista'>{item.conferencista}</td>
+                  <td className='agenda-salon'>{item.salon}</td>
                 </tr>
               ))}
             </tbody>
-
           </table>
         </div>
       ))}
